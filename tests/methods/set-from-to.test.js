2d7d90014fcaae067748fe245c62e8c3fecdb85b
saipi jasmin <jasmin.saipi@epitech.eu>
2019-10-07 10:30:41 +0200

    fix tests

--- tests/methods/set-from-to.test.js
+++ tests/methods/set-from-to.test.js
@@ -6,13 +6,13 @@ mwzDescribe(testSuites, () => {
     return () => {
       MapwizeUI.map({
         apiKey: '89a2695d7485fda885c96b405dcc8a25',
-        centerOnVenue: '56b20714c3fa800b00d8f0b5',
+        mapwizeOptions: {centerOnVenueId: '56b20714c3fa800b00d8f0b5'},
       }).then((map) => {
         map.on('mapwize:directionstart', direction => {
-          if (direction.from.placeId === '569f8d7cb4d7200b003c32a1' && direction.to.placeId === '5d08d8a4efe1d20012809ee5') {
+          if (direction.directions.from.placeId === '569f8d7cb4d7200b003c32a1' && direction.directions.to.placeId === '5d08d8a4efe1d20012809ee5') {
             window.callbackTest(null)
           } else {
-            window.callbackTest("Bad direction found. Expected from 569f8d7cb4d7200b003c32a1 to 5d08d8a4efe1d20012809ee5. Found from " + direction.from.placeId + " to " +direction.to.placeId)
+            window.callbackTest("Bad direction found. Expected from 569f8d7cb4d7200b003c32a1 to 5d08d8a4efe1d20012809ee5. Found from " + direction.directions.from.placeId + " to " +direction.directions.to.placeId)
           }
         })
         map.on('mapwize:venueenter', () => {
