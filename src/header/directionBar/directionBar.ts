1d8112f2835b5b1a71180c69e49eb36ea866f560
Sparksx <hollisprice.iii@gmail.com>
2019-12-18 10:36:02 +0100

    revert

--- src/header/directionBar/directionBar.ts
+++ src/header/directionBar/directionBar.ts
@@ -154,11 +154,9 @@ export class DirectionBar extends DefaultControl {
   // ---------------------------------------
 
   private _closeButtonClick (e: JQueryEventObject): void {
-    if (!this._map.headerManager.isSearchResultDisplayed()) {
-      this._clear()
-      this._map.headerManager.closeButtonClick()
-      $(this.map._container).find('.mapboxgl-ctrl-bottom-right').css('bottom', 0)
-    }
+    this._clear()
+    this._map.headerManager.closeButtonClick()
+    $(this.map._container).find('.mapboxgl-ctrl-bottom-right').css('bottom', 0)
   }
   private _reverseButtonClick (e: JQueryEventObject): void {
     let oldFrom = null
