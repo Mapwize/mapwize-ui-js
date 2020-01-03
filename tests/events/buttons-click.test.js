b3862ce0f215cdf3aa0e38a7ed3e106e0031a790
saipi jasmin <jasmin.saipi@epitech.eu>
2020-01-03 10:19:19 +0100

    update test

--- tests/events/buttons-click.test.js
+++ tests/events/buttons-click.test.js
@@ -22,11 +22,7 @@ mwzDescribe(testSuites, function () {
       }
     }).then(function (map) {
       map.on('mapwize:venueenter', function () {
-        setTimeout(function () {
-          map.setSelected(MAPWIZEPLACEID).then(function () {
-            $('#mwz-footer-informations-button').click()
-          })
-        }, 1000);
+        $('#mwz-footer-informations-button').click()
       })
     }).catch(function (e) { callbackTest(e); });
   })
