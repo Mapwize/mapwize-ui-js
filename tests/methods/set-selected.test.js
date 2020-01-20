fed383a3465b2cc353b2b644c2c3a069d884c060
Sparksx <hollisprice.iii@gmail.com>
2020-01-20 15:22:33 +0100

    improve tests

--- tests/methods/set-selected.test.js
+++ tests/methods/set-selected.test.js
@@ -5,20 +5,30 @@ mwzDescribe(testSuites, function () {
   mwzTest('With place id', function (callbackTest) {
     MapwizeUI.map({
       apiKey: APIKEY,
+      onSelectedChange: function (e) {
+        if (e._id === MAPWIZEPLACEID) {
+          callbackTest('Selected element must be mapwize place. Found id: ' + e._id)
+        } else {
+          callbackTest(null)
+        }
+      }
     }).then(function (map) {
-      return map.setSelected(MAPWIZEPLACEID).then(function () {
-        callbackTest(null)
-      })
+      return map.setSelected(MAPWIZEPLACEID)
     }).catch(function (e) { callbackTest(e) })
   })
 
   mwzTest('With placeList id', function (callbackTest) {
     MapwizeUI.map({
       apiKey: APIKEY,
+      onSelectedChange: function (e) {
+        if (e._id === BATHROOMPLACELISTID) {
+          callbackTest('Selected element must be bathroom placeList. Found id: ' + e._id)
+        } else {
+          callbackTest(null)
+        }
+      }
     }).then(function (map) {
-      return map.setSelected(BATHROOMPLACELISTID).then(function () {
-        callbackTest(null)
-      })
+      return map.setSelected(BATHROOMPLACELISTID)
     }).catch(function (e) { callbackTest(e) })
   })
 
