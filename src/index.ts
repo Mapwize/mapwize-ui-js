52dd5a821a15cb8dc7fdb536ac7b99d392e5b467
Sparks <hollisprice.iii@gmail.com>
2020-03-10 14:54:22 +0100

    fix: expose sdk version and display it in console (#138)

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import 'regenerator-runtime/runtime'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/tooltip'
 
+import packageJson from '../package.json'
+
 import './bootstrap-custom.scss'
 import './index.scss'
 
-import { Api, apiKey, apiUrl, Cache, config } from 'mapwize'
+import { Api, apiKey, apiUrl, Cache, config, version } from 'mapwize'
 import { AttributionControl, GeolocateControl, LngLat, LngLatBounds, Marker, NavigationControl, Point, Popup, ScaleControl, setRTLTextPlugin, Style } from 'mapwize'
 import { AccessKeyError, LoadError, MWZError, NoLocationError, VenueEnterError } from 'mapwize'
 
@@ -17,8 +19,15 @@ import { getUnits } from './measure'
 import { getLocales } from './translate'
 import { map } from './ui'
 
+const uiVersion = {
+  mapwize: version,
+  mapwizeUI: packageJson.version,
+}
+console.log('Mapwize UI v' + packageJson.version)
+
 export { map as default }
 export { map, uiConfig, config, apiKey, apiUrl }
+export { uiVersion as version }
 export { getUnits, getLocales }
 export { MWZError, AccessKeyError, LoadError, NoLocationError, VenueEnterError }
 export { Api, Cache, NavigationControl, GeolocateControl, ScaleControl, AttributionControl, Popup, Marker, Style, LngLat, LngLatBounds, Point, setRTLTextPlugin }
