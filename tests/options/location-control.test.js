98897d96b02994cb9e5d9996a655dd74e74732e4
saipi jasmin <jasmin.saipi@epitech.eu>
2020-03-11 13:30:29 +0100

    add tests

---
+++ tests/options/location-control.test.js
@@ -0,0 +1,30 @@
+const { mwzDescribe, mwzTest } = require('../core/utils')
+
+const testSuites = 'Location control'
+mwzDescribe(testSuites, function () {
+  mwzTest('show location control button ', function (callbackTest) {
+    MapwizeUI.map({
+      apiKey: APIKEY,
+      locationControl: true
+    }).then(function (map) {
+      if ($('.mwz-ctrl-location').length !== 1) {
+        callbackTest('.mwz-ctrl-location should exist when locationControl is set')
+      } else {
+        callbackTest(null)
+      }
+    }).catch(function (e) { callbackTest(e) })
+  })
+
+  mwzTest('location control button hidden by default ', function (callbackTest) {
+    MapwizeUI.map({
+      apiKey: APIKEY,
+      locationControl: false
+    }).then(function (map) {
+      if ($('.mwz-ctrl-location').length !== 0) {
+        callbackTest('.mwz-ctrl-location should not exist when locationControl is not set')
+      } else {
+        callbackTest(null)
+      }
+    }).catch(function (e) { callbackTest(e) })
+  })
+})
