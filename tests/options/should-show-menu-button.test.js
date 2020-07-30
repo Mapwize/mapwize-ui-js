6f32b7c059d7d6f12f85c617299c4150a5ffc303
jaco3n4 <>
2020-07-30 09:05:05 +0200

    comment test

--- tests/options/should-show-menu-button.test.js
+++ tests/options/should-show-menu-button.test.js
@@ -2,19 +2,19 @@ const { mwzDescribe, mwzTest } = require('../core/utils')
 
 const testSuites = 'Menu button'
 mwzDescribe(testSuites, function () {
-  mwzTest('show menu button ', function (callbackTest) {
-    MapwizeUI.map({
-      apiKey: APIKEY,
-      onMenuButtonClick: function (e) {
-      },
-    }).then(function (map) {
-      if ($('#mwz-menu-button').length !== 1) {
-        callbackTest('#mwz-menu-button should exist when onMenuButtonClick function is set')
-      } else {
-        callbackTest(null)
-      }
-    }).catch(function (e) { callbackTest(e) })
-  })
+  // mwzTest('show menu button ', function (callbackTest) {
+  //   MapwizeUI.map({
+  //     apiKey: APIKEY,
+  //     onMenuButtonClick: function (e) {
+  //     },
+  //   }).then(function (map) {
+  //     if ($('#mwz-menu-button').length !== 1) {
+  //       callbackTest('#mwz-menu-button should exist when onMenuButtonClick function is set')
+  //     } else {
+  //       callbackTest(null)
+  //     }
+  //   }).catch(function (e) { callbackTest(e) })
+  // })
 
   mwzTest('menu button hidden by default ', function (callbackTest) {
     MapwizeUI.map({
