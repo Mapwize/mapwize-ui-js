41e8639002786634c1743f91b924dc49e4e994eb
Sparks <hollisprice.iii@gmail.com>
2020-09-11 09:26:59 +0200

    add es locale (#204)

--- webpack.analyze.js
+++ webpack.analyze.js
@@ -1,12 +1,12 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
+const { merge } = require( 'webpack-merge' )
+const common = require( './webpack.common.js' )
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
- 
-module.exports = merge(common, {
+const BundleAnalyzerPlugin = require( 'webpack-bundle-analyzer' ).BundleAnalyzerPlugin
+
+module.exports = merge( common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
     new BundleAnalyzerPlugin()
   ]
-})
+} )
